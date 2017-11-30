e28fa7453f57e3087a8de57467ee53361e708da1
DileepSankhla <sankhla.dileep96@gmail.com>
2017-12-01 00:05:07 +0530

    generating the shortcut key for our executable

---
+++ shortcut.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+NAME = 'QuickNote'
+COMMAND = '/usr/bin/QuickNote'
+SHORTCUT = '<Ctrl><Shift>0'
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name \'"$NAME"\'
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command \'"$COMMAND"\'
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding \'"$SHORTCUT"\'
+
+
+
